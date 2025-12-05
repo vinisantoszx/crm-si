c8fcb4e230f594b1f0543d7d4e41a93ffe2ffaff
vinisantoszx <silvavynicius4417@gmail.com>
2025-12-05 14:39:35 -0300

    fix: Forçar criacao de tabelas no Gunicorn

--- app.py
+++ app.py
@@ -105,7 +105,8 @@ def logout():
     session.pop('user_id', None)
     return redirect(url_for('login'))
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
